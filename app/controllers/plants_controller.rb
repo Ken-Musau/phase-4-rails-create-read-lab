415e9a26860ee6b87e3378487a2d451f634877eb
Kennedy Musau <kennedy.musauu@gmail.com>
2023-08-12 22:41:16 +0300

    add update

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -6,17 +6,27 @@ class PlantsController < ApplicationController
   end
 
   def show
-    plant = Plant.all.find_by(id: params[:id])
+    plant = Plant.find_by(id: params[:id])
     if plant
       render json: plant, status: :ok
     else
-      render json: { error: "content not found" }
+      render json: { error: "plant not found" }, status: :not_found
     end
   end
 
   def create
     plant = Plant.create(plant_params)
     render json: plant, status: :created
+  end
+
+  def update
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.update(plant_params)
+      render json: plant, status: :accepted
+    else
+      render json: { error: "Plant not found" }, status: :not_found
+    end
   end
 
   private
